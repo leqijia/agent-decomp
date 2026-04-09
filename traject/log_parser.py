e638c036237e57014ef9fb90c989f14f33b11c30
Adithya <adithyave@gmail.com>
2026-04-08 22:54:59 -0700

    These are the first versions of the trajectory pipline with dummy data.

---
+++ traject/log_parser.py
@@ -0,0 +1,14 @@
+import sys
+import json
+from fields import output_mapper
+#. City -> city, cty -> city, city -> city  
+# input_fieldname -> output_fieldnam
+try:
+    with open(sys.argv[1],'r') as file:
+        data = json.load(file)
+        output = output_mapper()
+        output.normalize(data)
+except json.JSONDecodeError as e:
+    print("Malformed JSON:",e)
+except FileNotFoundError:
+    print("File not found error")
